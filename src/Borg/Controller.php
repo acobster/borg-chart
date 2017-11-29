fc5cd532937920d269e5d0d293065aef7ebf6c65
The Ambassador of Funk <acobster@gmail.com>
2017-11-29 00:42:02 -0800

    type annotation on ctrl ctor, comments

--- src/Borg/Controller.php
+++ src/Borg/Controller.php
@@ -3,12 +3,24 @@
 namespace Borg;
 
 class Controller {
+  /**
+   * @var string directory where view files live
+   */
   protected $viewDir;
 
-  public function __construct($viewDir) {
+  /**
+   * Constructor
+   * @param string $viewDir where to look for view files
+   */
+  public function __construct(string $viewDir) {
     $this->viewDir = $viewDir;
   }
 
+  /**
+   * Render the home page
+   * @param array $params the request params
+   * @return string the HTML to render
+   */
   public function homeAction(array $params) {
     $whitelist = ['s']; // this provides a simple way to support more params
     $whitelistedParams = array_intersect_key($params, array_flip($whitelist));
--- src/Borg/DbConnection.php
+++ src/Borg/DbConnection.php
@@ -7,6 +7,9 @@ use PDO;
 class DbConnection {
   protected $connection;
 
+  /**
+   * Constructor. Uses environment variables to create a reusable conneciton.
+   */
   public function __construct() {
     $dsn = sprintf(
       'mysql:dbname=%s;host=%s',
@@ -21,6 +24,12 @@ class DbConnection {
     );
   }
 
+  /**
+   * Query $sql on the Borg database, optionally binding $params
+   * @param string $sql the SQL to run
+   * @param array $params the params to bind to $sql
+   * @return array the results of the query
+   */
   public function query(string $sql, array $params = []) {
     $statement = $this->connection->prepare($sql);
     $statement->execute($params);
