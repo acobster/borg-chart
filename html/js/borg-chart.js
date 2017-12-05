51d657c5f678185fa7d8605cbb5a768aa1f839c8
The Ambassador of Funk <acobster@gmail.com>
2017-12-05 01:07:43 -0800

    fix search

--- html/js/borg-chart.js
+++ html/js/borg-chart.js
@@ -11,13 +11,17 @@
       .page.len(100)
       .draw();
 
-    $('.dataTables_filter').on('change keyup', function() {
+    $('.dataTables_filter input').on('change keyup', function() {
       // configure search to only match employee name
       $borgTable
-        .columns([0])
+        .columns(0)
         .search( $(this).val() )
+        // for strict matches only, disable "smart" search:
+        //.search( $(this).val(), false, false )
         .draw();
     });
+
+
   });
 
 
