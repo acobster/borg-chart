d97760f5f358550c9432560a310b4c4da0060931
The Ambassador of Funk <acobster@gmail.com>
2017-12-04 22:58:16 -0800

    display stuff all pretty

--- views/home.php
+++ views/home.php
@@ -3,15 +3,31 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="/css/style.css">
   </head>
   <body>
     <main role="main">
 
-      <?php foreach ($v['employees'] as $employee) : ?>
-        <p><?= implode(',', $employee) ?></p>
-      <?php endforeach; ?>
+      <h1>Borg Chart</h1>
 
-      <?php if (empty($v['employees'])) : ?>
+      <?php if (!empty($v['employees'])) : ?>
+
+        <table id="borg-employees">
+          <th>Employee Name</th>
+          <th>Boss Name</th>
+          <th>Distance from CEO</th>
+
+          <?php foreach ($v['employees'] as $employee) : ?>
+            <tr>
+              <td><?= $employee['name'] ?></td>
+              <td><?= $employee['boss_name'] ?></td>
+              <td><?= $employee['distance'] ?></td>
+            </tr>
+          <?php endforeach; ?>
+        </table>
+
+      <?php else : ?>
         <p>No employees found</p>
       <?php endif; ?>
 
